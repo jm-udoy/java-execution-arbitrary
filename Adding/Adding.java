6a8604df4c71970052995e27cda0a4554b6b35fa
JM Udoy <junayedibne.udoy@gmail.com>
2023-09-05 21:19:49 +0600

    Taking First Number Input

--- Adding/Adding.java
+++ Adding/Adding.java
@@ -1,9 +1,16 @@
 package Adding;
 
+import java.util.Scanner;
+
 public class Adding {
     public static void main(String[] args) {
 
-        System.out.println(5+10);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter First Number:");
+        int a = scan.nextInt();
+
+
+        System.out.println(a);
         
     }
 }
